cdac7342423625c425a7ba3fc30669c1108feeac
TatianaMartynova <2gabbrie@gmail.com>
2023-04-08 19:22:52 +0300

    HW7

---
+++ 7.async/task.js
@@ -0,0 +1,67 @@
+class AlarmClock {
+    constructor() {
+      this.alarmCollection = [];
+      this.intervalId = null;
+    }
+  
+    addClock(time, callback) {
+      if (!time || !callback) {
+        throw new Error('Отсутствуют обязательные аргументы');
+      }
+  
+      if (this.alarmCollection.some(alarm => alarm.time === time)) {
+        console.warn('Уже присутствует звонок на это же время');
+        return;
+      }
+  
+      this.alarmCollection.push({
+        time,
+        callback,
+        canCall: true,
+      });
+    }
+  
+    removeClock(time) {
+      const initialLength = this.alarmCollection.length;
+      this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+      return this.alarmCollection.length !== initialLength;
+    }
+  
+    getCurrentFormattedTime() {
+      const date = new Date();
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+      return `${hours}:${minutes}`;
+    }
+  
+    start() {
+      if (this.intervalId) {
+        return;
+      }
+  
+      this.intervalId = setInterval(() => {
+        const currentTime = this.getCurrentFormattedTime();
+  
+        this.alarmCollection.forEach(alarm => {
+          if (alarm.time === currentTime && alarm.canCall) {
+            alarm.canCall = false;
+            alarm.callback();
+          }
+        });
+      }, 1000);
+    }
+  
+    stop() {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  
+    resetAllCalls() {
+      this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+  
+    clearAlarms() {
+      this.stop();
+      this.alarmCollection = [];
+    }
+  }
